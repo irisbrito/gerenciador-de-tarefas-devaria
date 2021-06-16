35861f96bc16791ad1fe52e7e40b759743592999
Iris <irisbritox@gmail.com>
2021-06-16 17:34:35 -0300

    Criar projeto

---
+++ src/main/kotlin/com/devaria/todoapi/ToDoApiApplication.kt
@@ -0,0 +1,11 @@
+package com.devaria.todoapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ToDoApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<ToDoApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/devaria/todoapi/ToDoApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.devaria.todoapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ToDoApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
