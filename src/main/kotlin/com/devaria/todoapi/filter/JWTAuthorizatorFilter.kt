51456905de92e4589d77fae0aca21c7010888e76
Iris <irisbritox@gmail.com>
2021-06-17 11:43:43 -0300

    Adicionar método getAuthetication no filtro JWT

--- src/main/kotlin/com/devaria/todoapi/filter/JWTAuthorizatorFilter.kt
+++ src/main/kotlin/com/devaria/todoapi/filter/JWTAuthorizatorFilter.kt
@@ -2,10 +2,13 @@ package com.devaria.todoapi.filter
 
 import com.devaria.todoapi.authorization
 import com.devaria.todoapi.bearer
+import com.devaria.todoapi.entity.User
+import com.devaria.todoapi.impl.UserDetailsImpl
 import com.devaria.todoapi.util.JWTUtil
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.core.userdetails.UsernameNotFoundException
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter
 import javax.servlet.FilterChain
 import javax.servlet.http.HttpServletRequest
@@ -26,7 +29,19 @@ class JWTAuthorizatorFilter(authenticationManager: AuthenticationManager, val jw
     }
 
     private fun getAuthentication(authorization: String): UsernamePasswordAuthenticationToken{
+        val token = authorization.substring(7)
 
+        if(jwtUtil.isTokenValid(token)){
+            val idString = jwtUtil.getUserId(token)
+
+            if(!idString.isNullOrBlank() && !idString.isNullOrEmpty()){
+                val user = User(idString.toLong(), "Username", "admin@admin.com", "1234@")
+                val userImpl = UserDetailsImpl(user)
+
+                return UsernamePasswordAuthenticationToken(userImpl, null, userImpl.authorities)
+            }
+        }
+        throw  UsernameNotFoundException("Token inválido")
     }
 
 
